84d791eab0a11ac56ba21b268df82f815e5a3566
Jacob Kell Martin <119345707+Jacobmartin22@users.noreply.github.com>
2023-01-21 21:09:34 -0600

    Add files via upload

--- .js
+++ .js
@@ -1,1 +1,39 @@
+class MealItem {
+    constructor(name, price) {
+        this.name = name;
+        this.price = price;
+    }
+}
 
+class Meal {
+    constructor() {
+        this.items = [];
+    }
+
+    additem(item) {
+        this.items.push(item);
+    }
+
+    viewMeal() {
+        console.log("Here is the meal:")
+        for (let item of this.items) {
+            console.log(item.name + ": $" + item.price);
+        }
+    }
+
+    deleteItem(itemName) {
+        this.items = this.items.filter(item => item.name !== itemname);
+    }
+}
+
+const meal = new Meal();
+
+meal.additem(new MealItem("Burrito", 7));
+meal.additem(new MealItem("Fried Chicken", 10));
+meal.additem(new MealItem("Fries", 4));
+
+meal.viewMeal();
+
+meal.deleteItem("Burrito");
+
+meal.viewMenu
